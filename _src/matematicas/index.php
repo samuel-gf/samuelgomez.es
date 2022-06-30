fa57bc80fbe4855da3f389124125456b29cd5efe
Samuel Gómez <profesor_s@outlook.com>
2022-06-30 09:52:55 +0200

    Cambiado problemas por secundaria

---
+++ _src/matematicas/index.php
@@ -0,0 +1,21 @@
+<?php include('inc/cabecera_html.php'); ?>
+	<title>Matemáticas</title>
+	</head>
+<body>
+
+<?php include('inc/lib.php'); ?>
+<?php include('inc/cabecera_fixed.php'); ?>
+
+
+<main>
+<header><h1>Matemáticas</h1></header>
+
+<h2>Tabla de contenidos</h2>
+<ol>
+	<li><a href="problemas/3eso_01.html">3º ESO</li>
+	<li><a href="problemas/1bto_01.html">1º Bachillerato</li>
+</ol>
+
+
+</main>
+<?php include('inc/footer.php'); ?>
