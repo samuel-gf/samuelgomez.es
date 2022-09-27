6a0a5287b398c34c77fbb5bacbb5efce537827ac
Samuel Gómez <profesor_s@outlook.com>
2022-09-27 10:09:34 +0200

    Actualizados enlaces

--- _src/links.php
+++ _src/links.php
@@ -8,6 +8,7 @@
 <nav id="menu"><ul>
 		<h1>Enlaces</h1>
 		<li><a href="#top">Informática</a></li>
+		<li><a href="#educacion">Educación</a></li>
 	</ul>
 </nav>
 
@@ -26,9 +27,17 @@
 	desarrollador de software.</li>
 <li><a href="https://plantuml.com/es/" target="_blank">PlantUML</a> lenguaje específico para 
 	generar diagramas.</li>
-</ul>
 <li><a href="https://mermaid-js.github.io/mermaid/#/sequenceDiagram" target="_blank">Mermaid</a>
 	Otro lenguaje específico para generar diagramas.</li>
+<li><a href="https://godotengine.org/">Godot Engine</a></li>
+</ul>
+</section>
+
+<section>
+<h1><a name="educacion">Educación</a></h1>
+<ul>
+<li><a href="https://educagob.educacionyfp.gob.es/curriculo/nuevo-curriculo/curriculos-ccaa.html">Legislación educativa</a></li>
+</ul>
 </section>
 
 </main>
--- _src/links2.php
+++ _src/links2.php
@@ -7,7 +7,9 @@
 
 <nav id="menu"><ul>
 		<h1>Enlaces</h1>
-		<li><a href="#top">Laboral</a></li>
+		<li><a href="#top">Comunidad de Madrid</a></li>
+		<li><a href="#sindical">Sindical</a></li>
+		<li><a href="#otros">Otros</a></li>
 	</ul>
 </nav>
 
@@ -19,45 +21,32 @@
 
 <!------------------------------------------------------------------------------------->
 <section>
+<h1><a href="links2">Enlaces segunda parte</a></h1>
 
-<h1><a name="laboral">Laboral</a></h1>
+<h2><a href="cam">Comunidad de Madrid</a></h2>
+<ul>
+<li><a href="https://www.comunidad.madrid/servicios/educacion/personal-educacion">Personal educación</a></li>
+<li><a href="https://www.comunidad.madrid/servicios/educacion/profesorado-interino">Ares</a></li>
+<li><a href="https://tramita.comunidad.madrid/oferta-empleo/puestos-interinos-profesores-0">Ficha electrónica</a></li>
+</ul>
 
-<ul><li>
-	<a href="https://www.ugt-sp.es/sectores/ensenanza" target="_blank">
-	Enseñanza federal</a>
-</li></ul>
-
-<ul><li>
-	<a href="https://ugtspmadrid.es/category/ensenanza/" target="_blank">
-	Enseñanza CAM</a>
-</li></ul>
-<ul><li>
-	<a href="https://ugtspmadrid.es/category/ensenanza-privada/" target="_blank">
-	Enseñanza privada CAM</a>
-</li></ul>
-<ul><li>
-	<a href="https://ugtspmadrid.es/category/ensenanza-publica/" target="_blank">
-	Enseñanza pública CAM</a>
+<h2><a href="sindical">Sindical</a></h2>
+<ul>
+<li><a href="https://www.ugt-sp.es/sectores/ensenanza" target="_blank">Enseñanza federal</a></li>
+<li><a href="https://ugtspmadrid.es/category/ensenanza/" target="_blank">Enseñanza CAM</a></li>
+<li><a href="https://ugtspmadrid.es/category/ensenanza-privada/" target="_blank">Enseñanza privada CAM</a></li>
+<li><a href="https://ugtspmadrid.es/category/ensenanza-publica/" target="_blank">Enseñanza pública CAM</a></li>
+<li><a href="https://wa.me/653655977" target="_blank">Whatsapp interinos</a></li>
+<li><a href="https://wa.me/653655976" target="_blank">Whatsapp secundaria</a></li>
 </li></ul>
 
-<ul><li>
-	<a href="https://trabajarenlopublico.ning.com" target="_blank">
-	Trabaja en lo público</a>
-</li></ul>
-<ul><li>
-	<a href="https://serdugt.contigomas.com/offers/welcome" target="_blank">
-	Contigo Más</a>
-</li></ul>
+<h2><a href="otros">Otros servicios</a></h2>
+<ul>
+<li><a href="https://trabajarenlopublico.ning.com" target="_blank">Trabaja en lo público</a></li>
+<li><a href="https://serdugt.contigomas.com/offers/welcome" target="_blank">Contigo Más</a>
+</ul>
 
 
-<ul><li>
-	<a href="https://wa.me/653655977" target="_blank">
-	Whatsapp interinos</a>
-</li></ul>
-<ul><li>
-	<a href="https://wa.me/653655976" target="_blank">
-	Whatsapp secundaria</a>
-</li></ul>
 
 </section>
 
